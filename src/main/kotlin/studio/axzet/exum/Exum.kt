179d74513bee61e25b8f1d77f579bd71cbdfff96
FJReyna <franciscorgs@hotmail.es>
2022-08-08 03:07:00 -0500

    Added Incantatio Ore Generation

--- src/main/kotlin/studio/axzet/exum/Exum.kt
+++ src/main/kotlin/studio/axzet/exum/Exum.kt
@@ -5,6 +5,8 @@ import org.apache.logging.log4j.LogManager
 import org.apache.logging.log4j.Logger
 import studio.axzet.exum.block.ExumBlocks
 import studio.axzet.exum.item.ExumItems
+import studio.axzet.exum.world.feature.ModConfiguredFeatures
+import studio.axzet.exum.world.gen.ModOreGeneration
 
 
 @Suppress("UNUSED")
@@ -14,10 +16,14 @@ object Exum: ModInitializer {
     @JvmStatic val LOGGER: Logger = LogManager.getLogger(MOD_ID)
 
     override fun onInitialize() {
+        ModConfiguredFeatures.registerConfiguredFeatures()
+
         // Register Block
         ExumBlocks.registerModBlocks()
 
         // Register Items
         ExumItems.registerModItems()
+
+        ModOreGeneration.generateOres()
     }
 }
---
+++ src/main/kotlin/studio/axzet/exum/world/feature/ModConfiguredFeatures.kt
@@ -0,0 +1,49 @@
+package studio.axzet.exum.world.feature
+
+import net.minecraft.block.Blocks
+import net.minecraft.structure.rule.BlockMatchRuleTest
+import net.minecraft.util.registry.RegistryEntry
+import net.minecraft.world.gen.feature.*
+import studio.axzet.exum.Exum
+import studio.axzet.exum.block.ExumBlocks
+
+
+class ModConfiguredFeatures {
+    companion object {
+
+        private val OVERWORLD_INCANTATIO_ORES: List<OreFeatureConfig.Target> = listOf(
+            OreFeatureConfig.createTarget(OreConfiguredFeatures.STONE_ORE_REPLACEABLES, ExumBlocks.INCANTATIO_ORE.defaultState),
+            OreFeatureConfig.createTarget(OreConfiguredFeatures.DEEPSLATE_ORE_REPLACEABLES, ExumBlocks.DEEPSLATE_INCANTATIO_ORE.defaultState)
+        )
+
+        private val NETHER_INCANTATIO_ORES: List<OreFeatureConfig.Target> = listOf(
+            OreFeatureConfig.createTarget(OreConfiguredFeatures.BASE_STONE_NETHER, ExumBlocks.NETHERRACK_INCANTATIO_ORE.defaultState),
+        )
+
+        private val END_INCANTATIO_ORES: List<OreFeatureConfig.Target> = listOf(
+            OreFeatureConfig.createTarget(BlockMatchRuleTest(Blocks.END_STONE), ExumBlocks.ENDSTONE_INCANTATIO_ORE.defaultState),
+        )
+
+        val INCANTATIO_ORE: RegistryEntry<ConfiguredFeature<OreFeatureConfig, *>>? = ConfiguredFeatures.register(
+            "incantatio_ore",
+            Feature.ORE,
+            OreFeatureConfig(OVERWORLD_INCANTATIO_ORES, 9)
+        )
+
+        val NETHER_INCANTATIO_ORE: RegistryEntry<ConfiguredFeature<OreFeatureConfig, *>>? = ConfiguredFeatures.register(
+            "nether_incantatio_ore",
+            Feature.ORE,
+            OreFeatureConfig(NETHER_INCANTATIO_ORES,12)
+        )
+
+        val END_INCANTATIO_ORE: RegistryEntry<ConfiguredFeature<OreFeatureConfig, *>>? = ConfiguredFeatures.register(
+            "end_incantatio_ore",
+            Feature.ORE,
+            OreFeatureConfig(END_INCANTATIO_ORES,12)
+        )
+
+        fun registerConfiguredFeatures() {
+            Exum.LOGGER.debug("Registering the ModConfiguredFeatures for ${Exum.MOD_ID}")
+        }
+    }
+}
