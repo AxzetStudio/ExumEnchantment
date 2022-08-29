6ac8c0034de920a76ecdcd40f9e88ffc8b13dae1
NuniHacker <cedrick.albarran@gmail.com>
2022-08-28 17:59:04 -0700

    -Changue name "ancient_dirt" to "ancient_dirt_grass" -Added mineable and drop

--- src/main/kotlin/studio/axzet/exum/block/custom/AncientDirtBlockGrass.kt
+++ src/main/kotlin/studio/axzet/exum/block/custom/AncientDirtBlockGrass.kt
@@ -2,6 +2,6 @@ package studio.axzet.exum.block.custom
 
 import net.minecraft.block.Block
 
-class AncientDirtBlock:Block {
+class AncientDirtBlockGrass:Block {
     constructor(settings: Settings):super(settings)
 }
