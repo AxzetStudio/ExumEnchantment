7734636a3b28ede522fc35d58aa8ff1901c756e4
FJReyna <franciscorgs@hotmail.es>
2022-09-19 17:22:26 -0500

    Added LootTables to structures for Incantatio

--- src/main/kotlin/studio/axzet/exum/util/ExumLootTableModifiers.kt
+++ src/main/kotlin/studio/axzet/exum/util/ExumLootTableModifiers.kt
@@ -3,19 +3,15 @@ package studio.axzet.exum.util
 import net.fabricmc.fabric.api.loot.v2.LootTableEvents
 import net.fabricmc.fabric.api.loot.v2.LootTableEvents.Modify
 import net.fabricmc.fabric.api.loot.v2.LootTableSource
+import net.minecraft.item.Item
 import net.minecraft.loot.LootManager
 import net.minecraft.loot.LootPool
 import net.minecraft.loot.LootTable
-import net.minecraft.loot.condition.EntityPropertiesLootCondition
 import net.minecraft.loot.condition.RandomChanceLootCondition
-import net.minecraft.loot.context.LootContext
 import net.minecraft.loot.entry.ItemEntry
 import net.minecraft.loot.function.SetCountLootFunction
 import net.minecraft.loot.provider.number.ConstantLootNumberProvider
 import net.minecraft.loot.provider.number.UniformLootNumberProvider
-import net.minecraft.predicate.entity.EntityEquipmentPredicate
-import net.minecraft.predicate.entity.EntityPredicate
-import net.minecraft.predicate.item.ItemPredicate
 import net.minecraft.resource.ResourceManager
 import net.minecraft.util.Identifier
 import studio.axzet.exum.item.ExumItems
@@ -23,6 +19,42 @@ import studio.axzet.exum.item.ExumItems
 
 class ExumLootTableModifiers {
     companion object {
+
+        private val ABANDONED_MINESHAFT_CHEST_ID: Identifier = Identifier(
+            "minecraft",
+            "chests/abandoned_mineshaft"
+        )
+
+        private val BASTION_TREASURE_CHEST_ID: Identifier = Identifier(
+            "minecraft",
+            "chests/bastion_treasure"
+        )
+
+        private val DESERT_PYRAMID_CHEST_ID: Identifier = Identifier(
+            "minecraft",
+            "chests/desert_pyramid"
+        )
+
+        private val STRONGHOLD_LIBRARY_CHEST_ID: Identifier = Identifier(
+            "minecraft",
+            "chests/stronghold_library"
+        )
+
+        private val JUNGLE_TEMPLE_CHEST_ID: Identifier = Identifier(
+            "minecraft",
+            "chests/jungle_temple"
+        )
+
+        private val PILLAGER_OUTPOST_CHEST_ID: Identifier = Identifier(
+            "minecraft",
+            "chests/pillager_outpost"
+        )
+
+        private val MANSION_CHEST_ID: Identifier = Identifier(
+            "minecraft",
+            "chests/woodland_mansion"
+        )
+
         private val IGLOO_STRUCTURE_CHEST_ID: Identifier = Identifier(
             "minecraft",
             "chests/igloo_chest"
@@ -30,15 +62,35 @@ class ExumLootTableModifiers {
 
         fun modifyLootTable() {
             LootTableEvents.MODIFY.register(Modify { _: ResourceManager?, _: LootManager?, id: Identifier, tableBuilder: LootTable.Builder, _: LootTableSource? ->
-                if (IGLOO_STRUCTURE_CHEST_ID == id) {
-                    val poolBuilder: LootPool.Builder = LootPool.builder()
-                        .rolls(ConstantLootNumberProvider.create(1f))
-                        .conditionally(RandomChanceLootCondition.builder(1f)) // Drops 100% of the time
-                        .with(ItemEntry.builder(ExumItems.RAW_INCANTATIO))
-                        .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0f, 1.0f)).build())
-                    tableBuilder.pool(poolBuilder.build())
+
+                when(id) {
+                    ABANDONED_MINESHAFT_CHEST_ID -> tableBuilder.pool(customPoolBuilder(1f, 0.3f, ExumItems.INCANTATIO))
+                    BASTION_TREASURE_CHEST_ID    -> tableBuilder.pool(defaultPoolBuilder(ExumItems.INCANTATIO))
+                    DESERT_PYRAMID_CHEST_ID      -> tableBuilder.pool(customPoolBuilder(1f, 0.5f, ExumItems.INCANTATIO))
+                    STRONGHOLD_LIBRARY_CHEST_ID  -> tableBuilder.pool(defaultPoolBuilder(ExumItems.INCANTATIO))
+                    JUNGLE_TEMPLE_CHEST_ID       -> tableBuilder.pool(defaultPoolBuilder(ExumItems.INCANTATIO))
+                    PILLAGER_OUTPOST_CHEST_ID    -> tableBuilder.pool(defaultPoolBuilder(ExumItems.INCANTATIO))
+                    MANSION_CHEST_ID             -> tableBuilder.pool(defaultPoolBuilder(ExumItems.INCANTATIO))
+                    IGLOO_STRUCTURE_CHEST_ID     -> tableBuilder.pool(defaultPoolBuilder(ExumItems.INCANTATIO))
                 }
+
             })
+        }
+
+        private fun defaultPoolBuilder(item: Item): LootPool.Builder {
+            return LootPool.builder()
+                .rolls(ConstantLootNumberProvider.create(1f))
+                .conditionally(RandomChanceLootCondition.builder(1f)) // 100% drop rate
+                .with(ItemEntry.builder(item))
+                .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1f, 1f)).build())
+        }
+
+        private fun customPoolBuilder(quantity: Float, chance: Float, item: Item): LootPool.Builder {
+            return LootPool.builder()
+                .rolls(ConstantLootNumberProvider.create(quantity))
+                .conditionally(RandomChanceLootCondition.builder(chance))
+                .with(ItemEntry.builder(item))
+                .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1f, 1f)).build())
         }
     }
 }
